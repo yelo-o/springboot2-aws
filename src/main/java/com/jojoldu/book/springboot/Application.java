e814ac8c344e285362bab8465cb28e036753fdbf
yelo-o <flyordig@gmail.com>
2023-09-09 13:26:46 +0900

    배포 자동화 구성 - application.yml 수정 : 배포 자동화 구성 전, 새로운 파일이 commit되면 이전 버전은 중단시킨 후 업데이트된 jar을 실행하기 위해 먼저 pid commend 설정 - Application.java 수정 : 설정한 값을 pid commend로 사용하기 위해 main을 수정 - scripts/deploy.sh 추가 - gradle-publish.yml 수정 : 배포 패키지 생성 - appsec.yml 수정 : codedeploy가 자동으로 배포할 수 있도록

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -2,13 +2,17 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing //JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+        SpringApplication application = new SpringApplication(Application.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
