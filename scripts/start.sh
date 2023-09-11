62b5fc9d48fbbbb99735eda6c80681df05c5b7f6
yelo-o <flyordig@gmail.com>
2023-09-11 23:02:05 +0900

    profile -> profiles

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,6 @@ echo "> cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/"
 
 cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
 
-
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
@@ -30,5 +29,5 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,classpath:/application-$IDLE_PROFILE.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
-  -Dspring.profile.active=$IDLE_PROFILE \
+  -Dspring.profiles.active=$IDLE_PROFILE \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
