db7da0910efa44f15cba4bbe25454ad998fcc282
yelo-o <flyordig@gmail.com>
2023-09-11 17:45:44 +0900

    스크립트 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=springboot2-aws #해당 위치에 properties에 작성한 프로젝트명과 동일하게 작성합니다.
+PROJECT_NAME=springboot2-aws #해당 위치에 yml에 작성한 프로젝트명과 동일하게 작성합니다.
 
 echo "> Build 파일 복사"
 cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY/
@@ -28,6 +28,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
+  -Dspring.config.location=classpath:/application.yml,classpath:application-real.yml/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
   -Dspring.profile.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
