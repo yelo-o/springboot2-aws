900190fc7e103a089364f35ff8f00070d8d22d8f
yelo-o <flyordig@gmail.com>
2023-09-11 21:26:05 +0900

    real1, real2 프로파일 생성

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # 쉬고 있는 profile 찾기: real1이 사용 중이면, real2가 쉬고 있고, 반대면 real1이 쉬고 있음
-function find_idle_profile() {
+function find_idle_profile()
+{
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
   if [ ${RESPONSE_CODE} -ge 400 ] #400 보다 크면
@@ -23,7 +24,8 @@ function find_idle_profile() {
 }
 
 # 쉬고 있는 profile의 port 찾기
-function find_idle_port() {
+function find_idle_port()
+{
   IDLE_PROFILE=$(find_idle_profile)
 
   if [ ${IDLE_PROFILE} == real1 ]
